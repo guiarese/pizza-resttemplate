e1a166afafe23d5d24f6c6c1f6241d067ac89554
guiarese <guilhermearesende1@gmail.com>
2021-01-15 22:11:02 -0300

    first commit

---
+++ src/main/java/br/com/pizza/pizzaresttemplate/PizzaResttemplateApplication.java
@@ -0,0 +1,13 @@
+package br.com.pizza.pizzaresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaResttemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pizza/pizzaresttemplate/PizzaResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pizza.pizzaresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaResttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
